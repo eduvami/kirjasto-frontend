7d061cbf4a74c2bac7f3c1b1af5fed66779e3603
Tea Salonsaari <salotea@digitalentshelsinki.internal>
2021-10-18 15:00:41 +0300

    created basic structure for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import bookService from './services/book' 
+import Rating from '@mui/material/Rating'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BookInfo = (props) => {
+    
+    return (
+        <Rating name="half-rating-read" value={props.ratingValue} precision={0.5} readOnly />
+    )
+} 
+
+
+
+const Comments = (props) => {
+
 }
 
-export default App;
+const Recommendations = (props) => {
+
+}
+
+const 
+
+const App = () => {
+    const [rating, setRating] = useState('')
+
+
+
+    return (
+        <div>
+            <BookInfo ratingValue={rating}/>
+            
+        </div>
+    )
+}
+
+export default App
